dc153c60a35c66e3a511949e20828dd11ef2a2c7
Aidan Bane <aidanfbane@gmail.com>
2017-08-15 17:22:51 -0700

    (feat) All tests for helpers passing

--- src/lib/todoHelpers.js
+++ src/lib/todoHelpers.js
@@ -1,3 +1,16 @@
 export const addTodo = (todos, newItem) => todos.concat(newItem)
 
 export const newId = () => Math.floor(Math.random() * 1000000)
+
+export const findById = (id, todos) => todos.find(todo => todo.id === id)
+
+export const toggleTodo = (todo) => ({...todo, isComplete: !todo.isComplete})
+
+export const updateTodo = (todos, updatedTodo) => {
+  const updatedIndex = todos.findIndex(todo => todo.id === updatedTodo.id)
+  return [
+    ...todos.slice(0, updatedIndex),
+    updatedTodo,
+    ...todos.slice(updatedIndex + 1)
+  ]
+}
